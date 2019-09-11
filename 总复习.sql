cb27eb24256029c1196744cfd3ec31c6f95fbf60
Qianfugen <287541326@qq.com>
2019-09-11 21:02:12 +0800

    all

--- 总复习.sql
+++ 总复习.sql
@@ -629,4 +629,196 @@ begin
       close cs_emp;
 end;
 
+----------------------------------------------------------------------------------------
+
+--存储过程:可以存储在数据库中的过程
+--根据姓名查询工资
+
+create or replace procedure query_sal_by_empno(
+  /*
+       该存储过程需要的参数
+                 1. 数据类型:数据库常用的数据类型比如number varchar2 date.....
+                 2. 参数类型
+                    in:只能用来往存储过程中传入参数
+                    out:只能从存储过程中中带出执行过程中产生的结果
+                    inout:既能传入又能带出数据
+       */
+    in_empno in emp.empno%type,
+    out_sal out emp.sal%type    
+)
+is 
+       --或者as
+       --该部分用来定义存储过程中需要用到的变量
+    v_i  number(5) := 5;
+begin
+    select sal into out_sal from emp where empno=in_empno;
+end;
+
+--调用存储过程必须要使用过程
+declare 
+    out_sal emp.sal%type;
+begin
+    query_sal_by_empno(7369,out_sal);
+    dbms_output.put_line(out_sal);
+end;
+
+--返回行类型数据:根据用户名查询用户的所有信息
+create or replace procedure query_all_by_name(
+       in_name in emp.ename%type,
+       emp_row out emp%rowtype
+)
+is     
+begin
+       select * into emp_row from emp where ename=in_name;
+end;
+
+--调用过程
+declare
+       emp_row emp%rowtype;
+begin
+       query_all_by_name('KING',emp_row);
+       dbms_output.put_line('编号:'||emp_row.empno||',姓名:'||emp_row.ename||',薪资:'||emp_row.sal);
+end;
+
+--根据用户名称模糊查询所有员工信息
+create or replace procedure query_by_name_like(
+       in_name in emp.ename%type,
+       emp_cur out sys_refcursor
+)
+is
+begin
+       open emp_cur for select * from emp where ename like '%'||in_name||'%';
+end;
+
+declare
+       emp_cur sys_refcursor;
+       emp_row emp%rowtype;
+begin
+       query_by_name_like('S',emp_cur);
+       loop
+               fetch emp_cur into emp_row;
+               exit when emp_cur%notfound;
+               dbms_output.put_line('编号:'||emp_row.empno||',姓名:'||emp_row.ename||',薪资:'||emp_row.sal);
+       end loop;
+       close emp_cur;
+end;
+
+--查询所有员工
+--方式一
+create or replace procedure query_all_emp(
+       emp_cur out sys_refcursor
+)
+is
+begin
+       open emp_cur for select * from emp;
+        
+end;
+
+declare 
+       emp_cur sys_refcursor;
+       emp_row emp%rowtype;
+begin
+       query_all_emp(emp_cur);
+       loop
+               fetch emp_cur into emp_row;
+               exit when emp_cur%notfound;
+               dbms_output.put_line('编号:'||emp_row.empno||',姓名:'||emp_row.ename||',薪资:'||emp_row.sal);
+       end loop;    
+       close emp_cur;
+end;
+
+--方式二
+create or replace procedure query_all_emp2
+is
+       emp_cur sys_refcursor;
+       emp_row emp%rowtype;
+begin
+       open emp_cur for select * from emp;
+       loop
+          fetch emp_cur into emp_row;
+          exit when emp_cur%notfound;  
+          dbms_output.put_line('编号:'||emp_row.empno||',姓名:'||emp_row.ename||',薪资:'||emp_row.sal);
+       end loop;
+       close emp_cur;
+end;
+
+declare
+begin
+       query_all_emp2;     
+end;
+
+
+/*
+   方法  
+       1. 必须要有返回值
+       2. 可以直接在sql语句中调用     
+       3. 过程可以返回任意数据类型以及任意多的结果,方法同样可以返回任意类型但是只能返回一个结果   
+   使用存储过程与函数的原则:
+       (1).如果需要返回多个值和不返回值，就使用存储过程；如果只需要返回一个值，就使用函数。
+       (2).存储过程一般用于执行一个指定的动作，函数一般用于计算和返回一个值。
+       (3).可以在SQL语句内部（如表达式）调用函数来完成复杂的计算问题，但不能调用储存过程。所以这是函数的特色。
+   使用存储过程或者函数的优点:
+       (1).共同使用的代码可以只需要被编写和测试一次，就可以被需要该代码的任何程序调用
+       (2).这种集中编写、集中维护更新、大家共享（或重用）的方法，简化了应用程序的开发和维护，提高效率与性能。
+       (3).这种模块化的方法使得一个复杂的问题、大的程序逐步简化成几个简单的、小的程序部分，进行分别编写、调试。因此程序的结构更加清晰、简单、也容易实现。
+       (4).可以在各个开发者间提供处理数据、控制流程、提示信息等方面的一致性。
+       (5).节省内存空间。它们以一种压缩的形式被储存在外存中，当被调用的时才被放入内存进行处理。并且如果多个用户要执行相同的储存过程或函数时就只需要在内存中加载一个该存储过程或函数。
+       (6).提高数据的安全性与完整性。通过把一些对数据的操作放到存储过程或函数中，就可以通过是否授予用户有执行该过程或函数的权限，来限制某些用户对数据进行这些操作
+*/
+
+--计算薪资+奖金的函数
+create or replace function add_sal_and_comm(
+       in_empno in emp.empno%type
+)return emp.sal%type
+is
+       sal_and_comm emp.sal%type;
+begin
+       select (sal+nvl(comm,0)) into sal_and_comm from emp where empno=in_empno;
+       return sal_and_comm;
+end;
+
+--sql语句直接调用
+select * from emp;
+select add_sal_and_comm(7499) 总收入 from dual;
+
+--使用过程调用
+declare
+       sal_and_comm emp.sal%type;
+begin
+       sal_and_comm := add_sal_and_comm(7499);
+       dbms_output.put_line('总收入:'||sal_and_comm);
+end;
+
+-----------------------------------------------------------------------------------------------------------------
+
+/*
+  程序包:类似于java里面的接口
+    包的定义:可以定义全局变量,可以定义游标类型,定义存储过程或者函数
+*/
+
+/*
+  给声明好的包定义编写主体
+*/
+
+--声明函数
+create or replace package pack_emp
+is
+       function add_sal_and_comm(v_empno emp.ename%type) return emp.sal%type;
+end;
+
+--编写主体
+create or replace package body pack_epm
+is
+       --给add_sal_and_comm定义主体
+       create or replace function add_sal_and_comm(
+              in_empno in emp.empno%type
+       )return emp.sal%type
+       is
+               in_empno emp.empno%type;
+               sal_and_comm emp.sal%type;
+       begin
+               select (sal+nvl(comm,0)) into sal_and_comm from emp where empno=in_empno;
+               return sal_and_comm;
+       end add_sal_and_comm;
+end;
 
